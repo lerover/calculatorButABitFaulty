fc733c0bc052ba49be0c66ed171998c284a89efc
lerover1155@gmail.com <lerover1155@gmail.com>
2023-03-28 10:07:44 +0630

    add

--- calculator.js
+++ calculator.js
@@ -3,10 +3,16 @@ const numberEl1 = document.querySelector('.input_number1')
 
 const resultEl = document.querySelector('.result')
 const show_operatorEl = document.querySelector('.show_operator')
+
+const no1El = document.querySelector('.no1')
+const no2El = document.querySelector('.no2')
+const no3El = document.querySelector('.no3')
+const no4El = document.querySelector('.no4')
+
 function add() {
    show_operatorEl.textContent = '+'
    const result = Number(numberEl.value) + Number(numberEl1.value)
-  
+
    resultEl.textContent = result
 }
 
@@ -28,3 +34,19 @@ function divide() {
    resultEl.textContent = result
 }
 
+function number() {
+   numberEl.value = no1El.textContent
+
+}
+
+function number1() {
+   numberEl.value = no2El.textContent
+}
+
+function number2() {
+   numberEl.value = no3El.textContent
+}
+
+function number3() {
+   numberEl.value = no4El.textContent
+}
